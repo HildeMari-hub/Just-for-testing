872d0868fd4e651498d49dff21d9d9251e38da1e
HildeMari-hub <hildemarif@gmail.com>
2025-04-18 17:31:27 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+// Testing
+console.log (`Hello world`);
+
+// Test 2
+console.log (`Javascript is alive`);
+
+// Test 3
+console.log (`Fucka youuuu`);
+
+// Test 4
+// Cats has paws, can Hiss and has Fur
+var cat = {
+hasPaws: true, 
+canHiss: true, 
+hasFur: true   
+}
+// Inheritance
+var lion2 = Object.create(cat);
+console.log ("lion2:", lion2);
+
+console.log ("lion2 has Paws:", lion2. hasPaws); 
+console.log ("lion2 can Hiss:", lion2. canHiss);
+console.log ("lion2 has Fur:", lion2. hasFur);
+
+var lion2 = Object.create(cat);
+
+// Test 5
+
+
+var veggies = []
+veggies.push('parsley')
+veggies.push('carrot')
+console.log(veggies[2])
